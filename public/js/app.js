75118bf23799916cc03f65e0e59a1ac858f607d1
shadykh <eng.shady94@gmail.com>
2021-06-04 18:21:08 +0300

    Fix the code, v1.3

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,5 @@
-
 'use strict';
 
-console.log('🚀 ~ file: app.js ~ line 15 ~ $ ~ playerName');
-const message = document.querySelector('.message');
-const content = document.querySelector('.content');
-
-//const url = new URL(window.location.href);
-//const playerName = url.searchParams.get('username');
-
-
-
-//message.innerHTML = `Don't worry <strong>${playerName}</strong>, your opponent is coming. Here's some random facts to pass the time.`;
 const socket = io();
 
 $('#new').on('click', () => {
@@ -21,7 +10,7 @@ $('#new').on('click', () => {
     return;
   }
   console.log('connection playerName ',playerName );
-  socket.emit('createGame', { playerName });
+  socket.emit('join', { playerName });
 });
 
 $('#join').on('click', () => {
@@ -31,60 +20,5 @@ $('#join').on('click', () => {
     alert('Please enter your name and game ID.');
     return;
   }
-  socket.emit('joinGame', { name:name, room: roomID });
+  socket.emit('newJoin', { name:name, room: roomID });
 });
-
-/* 
-const socket = io();
-
-socket.on('connect', () => {
-  socket.emit('waiting', { playerName });
-}); */
-
-socket.on('game-update', (msg) => {
-  const { game, yourTurn, otherPlayer } = msg;
-  const turnMessage = yourTurn
-    ? `Your Turn`
-    : `Waiting for other player to make move`;
-  content.innerHTML = ` ${turnMessage}
-                    <div class='game'></div>
-                ${otherPlayer}`;
-  const grid = document.querySelector('.game');
-  grid.innerHTML = '';
-  console.log(game);
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      let letter = ' ';
-      if (game[i][j] === 1) letter = 'X';
-      else if (game[i][j] === 25) letter = 'O';
-      grid.innerHTML += `<div class='game-block'>${letter}</div>`;
-    }
-  }
-  if (yourTurn) {
-    const gameblocks = document.querySelectorAll('.game-block');
-    gameblocks.forEach((block, index) => {
-      block.addEventListener('click', () => makeMove(index));
-    });
-  }
-});
-
-socket.on('player-disconnected', ({ message }) => {
-  alert(message);
-  document.location.href = '/';
-});
-
-socket.on('server-message', (message) => {
-  console.log(message);
-});
-
-socket.on('game-over', (obj) => {
-  console.log(obj);
-  alert(obj.message);
-  document.location.href = '/';
-});
-
-const makeMove = (move) => {
-  console.log(move);
-  socket.emit('move', { move });
-};
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,174 +1,52 @@
 'use strict';
 
-require('dotenv').config();
-
 const express = require('express');
-
 const app = express();
-
-const PORT = process.env.PORT || 4000;
-
-const socketIo = require('socket.io');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-  
-app.get('/waiting', (req, res) => {
-  res.sendFile(__dirname + '/public/waiting.html');
-});
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => console.log('listening on port ' + PORT));
+const io = require('socket.io')(server);
+const path = require('path');
+const Game = require('./models/game');
+const root = path.join(__dirname, 'public');
+const faker = require('faker');
 
-const Player = require('./Models/player/player.js');
-const TicTacToe = require('./Models/game/game.js');
+const symbol = ['X', 'O'];
+const numbers = [1, -1];
+
+app.use(express.static(root));
+app.get('/' , (req, res) => {
+  res.sendFile('index.html', {root});
+});
 
 
-let waitingList = [];
-let games = [];
-let players = [];
-
-let rooms = 0;
-
-const io = socketIo(server);
-
-let room = io.of('http://localhost:3000/waiting');
-
+let rooms = []; 
+let archivedRooms = []; 
 
 io.on('connection', (socket) => {
-  const updateGame = (game) => {
-    const player1 = players.find((player) => player.playerId === game.player1Id);
-    const player2 = players.find((player) => player.playerId === game.player2Id);
-    if (game.turn === 0) {
-      io.to(`room-${++rooms}`).emit('game-update', {
-        game: game.game,
-        yourTurn: true,
-        otherPlayer: player2.playerName,
-      });
-      io.to(room).emit('game-update', {
-        game: game.game,
-        yourTurn: false,
-        otherPlayer: player1.playerName,
-      });
+  let game;
+  socket.on('join', (playerName) => {
+    if (rooms.length === 0 || rooms[rooms.length-1].getPlayersCount() === 2) {
+      game = new Game(faker.datatype.number());
+      rooms.push(game);
+      console.log(`Total Rooms ${rooms.length}`);
     } else {
-      io.to(room).emit('game-update', {
-        game: game.game,
-        yourTurn: false,
-        otherPlayer: player2.playerName,
-      });
-      io.to(room).emit('game-update', {
-        game: game.game,
-        yourTurn: true,
-        otherPlayer: player1.playerName,
-      });
+      game = rooms[rooms.length - 1];
     }
-  };
-  socket.on('waiting', ({ data }) => {
-    if (waitingList.length > 0) {
-      socket.join(`room-${++rooms}`);
-      const player2 = new Player(socket.id+1, data.name);
-      // socket.broadcast.to(data.room).emit('player1', {});
-      // socket.emit('player2', { name: data.name, room: data.room });
-      players.push(player2);
-      const game = new TicTacToe(players[0].playerId,players[1].playerId);
-      console.log('game start');
-      console.log(waitingList);
-      games.push(game);
-      console.log('new game');
-      console.log(games);
-      
-      updateGame(game);
-    } else {
-      waitingList.push(data.name);
-      console.log('new waiting person');
-      console.log('aaaaaaaa',waitingList);
-    }
-    //socket.emit('newGame', { name: playerName, room: `room-${rooms}` });
-    
-  });
-  socket.on('createGame', ({ playerName }) => {
-    console.log('createGame playerName',playerName);
-    const player = new Player(socket.id, playerName);
-    socket.join(room);
-    console.log(`room-${++rooms}`);
-    players.push(player);
-    socket.emit('waiting', { name: playerName, room: `room-${rooms}` });
-  });
-  
-  socket.on('joinGame', function (data) {
-    socket.emit('waiting', { name: data.name, room: data.room });
-    //.adapter.rooms[data.room];
-   
-
-    //console.log('game', game);
-    //updateGame(game);
-    /* players.push(player);
-    if (waitingList.length > 0) {
-      const newPlayer = waitingList.shift();
-      console.log('game start');
-      console.log(waitingList);
-      const game = new TicTacToe(player.playerId, newPlayer.playerId);
-      games.push(game);
-      console.log('new game');
-      console.log(games);
-      updateGame(game);
-    } else {
-      waitingList.push(player);
-      console.log('new waiting person');
-      console.log('aaaaaaaa',waitingList);
-    } */
-  });
-  socket.on('move', ({ move }) => {
-    const game = games.find((game) => game.includes(socket.id));
-    if (game.yourTurn(socket.id)) {
-      console.log(game.makeMove(move));
-      updateGame(game);
-    } else {
-      socket.emit('server-message', {
-        message: 'Wait your turn',
-      });
-    }
-    const winner = game.winner();
-    if (winner) {
-      if (winner === 1) {
-        io.to(game.player1Id).emit('game-over', {
-          message: 'YOU WIN!',
-        });
-        io.to(game.player2Id).emit('game-over', {
-          message: 'YOU LOSE :(',
-        });
-      } else if (winner === 2) {
-        io.to(game.player2Id).emit('game-over', {
-          message: 'YOU WIN!',
-        });
-        io.to(game.player1Id).emit('game-over', {
-          message: 'YOU LOSE :(',
-        });
-      } else {
-        io.to(game.player1Id).to(game.player2Id).emit('game-over', {
-          message: 'SCRATCH, you\'re both losers',
-        });
-      }
+    socket.roomId = game.getRoomId();
+    socket.join(socket.roomId);
+    game.addPlayer(playerName, socket.id, socket, symbol[game.getPlayersCount()], numbers[game.getPlayersCount()]);
+    if (game.getPlayersCount() === 1) {
+      socket.emit('message', `Welcome ${playerName.playerName}, waiting another Player, your room id is ${socket.roomId}`);
     }
   });
-
-  socket.on('disconnect', () => {
-    const game = games.find((game) => game.includes(socket.id));
-    const find = waitingList.find((player) => player.id === socket.id);
-    players = players.filter((player) => player.id !== socket.id);
-    if (find) {
-      waitingList = waitingList.filter((player) => player.id !== socket.id);
-      console.log('left waiting area');
-      console.log(waitingList);
-    }
-    if (game) {
-      games = games.filter((g) => g.gameId !== game.gameId);
-      console.log('game over');
-      console.log(games);
-      io.to(game.player1Id).to(game.player2Id).emit('player-disconnected', {
-        message: 'Other player disconnected',
-      });
+  socket.on('newJoin', (data) => {
+    game = rooms[rooms.length - 1];
+    socket.roomId = data.room;
+    socket.join(socket.roomId);
+    game.addPlayer(data.name, socket.id, socket, symbol[game.getPlayersCount()], numbers[game.getPlayersCount()]);
+    if (game.getPlayersCount() === 2) {
+      io.to(socket.roomId).emit('empty-message', 'Game Starts');
+      game.setStatus(1); // In progress
     }
   });
 });
